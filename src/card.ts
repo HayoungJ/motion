9503cd0c2e5dddf959f29730b30488ce2d3822e1
Hayoung Jeon <hayoungj0710@gmail.com>
2021-04-05 19:49:37 +0900

    Add drag event
    add drag event for cards: dragging cards will now change its order

--- src/card.ts
+++ src/card.ts
@@ -9,14 +9,49 @@ type FormData = {
   content: string;
 };
 type ContentType = 'image' | 'video' | 'note' | 'todo';
+type OrderOfNode = {
+  node: HTMLElement;
+  order: number;
+};
 
 export default class Card implements CardMaker {
   private static cardsSection = document.querySelector('.cards');
-  constructor() {}
+  private cardId: number = 0;
+  private dragTarget?: HTMLElement;
+  constructor() {
+    Card.cardsSection?.addEventListener('dragstart', (event: Event) => {
+      const target = (event.target as HTMLElement).closest(
+        '.card'
+      ) as HTMLElement;
 
-  add(data: FormData, contentType: ContentType) {
+      if (!target) return;
+      target.style.zIndex = '100';
+      this.dragTarget = target;
+    });
+    Card.cardsSection?.addEventListener('dragend', (event: Event) => {
+      const target = (event.target as HTMLElement).closest(
+        '.card'
+      ) as HTMLElement;
+
+      if (target) target.style.zIndex = '0';
+    });
+    Card.cardsSection?.addEventListener('dragover', function (event) {
+      event.preventDefault();
+    });
+    Card.cardsSection?.addEventListener('drop', (event: Event) => {
+      event.preventDefault();
+      const target = (event.target as HTMLElement).closest(
+        '.card'
+      ) as HTMLElement;
+
+      target && this.drop(target);
+    });
+  }
+
+  add = (data: FormData, contentType: ContentType) => {
     const card = document.createElement('article');
     card.setAttribute('class', 'card');
+    card.setAttribute('draggable', 'true');
 
     const title = document.createElement('span');
     title.setAttribute('class', 'card__title');
@@ -54,13 +89,13 @@ export default class Card implements CardMaker {
     deleteButton.innerHTML = `<i class="fas fa-minus-circle" data-type="delete"></i>`;
 
     card.append(title);
-    card.append(desc);
     card.append(content);
+    card.append(desc);
     card.append(deleteButton);
     if (!Card.cardsSection)
       throw new Error('html tag of cards section has problem');
     Card.cardsSection.append(card);
-  }
+  };
 
   delete(card: Element) {
     card.remove();
@@ -84,4 +119,42 @@ export default class Card implements CardMaker {
       content.append(item);
     }
   }
+
+  private drop = (target: HTMLElement) => {
+    if (!Card.cardsSection) throw new Error('no card section');
+
+    const nodeList = Card.cardsSection.querySelectorAll('.card');
+    if (!nodeList) return;
+
+    let cards: OrderOfNode[] = [];
+    let oldTargetOrder: number | null = null;
+    let newTargetOrder: number | null = null;
+    nodeList.forEach((v, i) => {
+      if (v === this.dragTarget) oldTargetOrder = i;
+      else if (v === target) newTargetOrder = i;
+    });
+    if (newTargetOrder === null || oldTargetOrder === null)
+      throw new Error('there are no targets');
+
+    if (newTargetOrder === nodeList.length) newTargetOrder++;
+    for (let i = 0, max = nodeList.length; i < max; i++) {
+      if (oldTargetOrder === newTargetOrder) return;
+      else if (oldTargetOrder < newTargetOrder) {
+        if (i > newTargetOrder)
+          cards.push({ node: nodeList[i] as HTMLElement, order: i });
+        else cards.push({ node: nodeList[i] as HTMLElement, order: i - 1 });
+      } else {
+        if (i < newTargetOrder)
+          cards.push({ node: nodeList[i] as HTMLElement, order: i });
+        else cards.push({ node: nodeList[i] as HTMLElement, order: i + 1 });
+      }
+    }
+    cards[oldTargetOrder] = { ...cards[oldTargetOrder], order: newTargetOrder };
+    cards.sort((a: OrderOfNode, b: OrderOfNode): number => a.order - b.order);
+
+    Card.cardsSection.innerHTML = '';
+    for (let i = 0, max = cards.length; i < max; i++) {
+      Card.cardsSection.append(cards[i].node);
+    }
+  };
 }
